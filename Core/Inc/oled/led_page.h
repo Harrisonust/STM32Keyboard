8ce0a576c6f902dabc025631de1d199f652221d3
almost1234 <58636438+almost1234@users.noreply.github.com>
2021-11-07 13:31:17 +0800

    added intergraton of LED between ws2812

--- Core/Inc/oled/led_page.h
+++ Core/Inc/oled/led_page.h
@@ -9,18 +9,24 @@
 #define INC_LED_PAGE_H_
 
 #include "page_template.h"
+#include "RGB.h"
 
 #define min(a,b) (a > b) ? b : a
 #define max(a,b) (a > b) ? a : b
 
+#define SELECTION 4
+
+extern WS2812 ws2812;
+
 char *color[20] = {
+		"bright",
 		"red",
 		"green",
 		"blue"
 };
 
-uint8_t value[3] = {0};
-// insert init value handler in here
+//uint8_t value[3] = {0};
+//// insert init value handler in here
 
 void led_update();
 void led_onclick(char *combination, int charNum);
--- Core/Src/oled/led_page.c
+++ Core/Src/oled/led_page.c
@@ -7,49 +7,70 @@
 #include "../../Inc/oled/led_page.h"
 #include "math.h"
 
-int num_selection = 3;
-int current_selection = 0;
 
 PAGE led = {
 		.title = "LED",
 		.update_page = led_update,
 		.on_click= led_onclick};
 
+int current_led = 0;
+int current_selection = 0;
+
 void led_update(){
-	//try to init here
-	led_page_update();
+	char temp[20] = " ";
+
+	sprintf(temp, "LED : %d", current_led + 1);
+	ssd1306_WriteString(temp, Font_6x8, White);
+
+	for(int i = 0; i < 4; i++){
+		sprintf(temp,
+				i == current_selection ? "%s	<-- %d" :"%s	    %d",
+				color[i], ws2812.LED_Data[current_led][i]);
+		ssd1306_SetCursor(0, 20 + 15* (i));
+		ssd1306_WriteString(temp, Font_6x8, White);
+	}
 }
 
 void led_onclick(char *combination, int charNum){
 	// insert different keybinds combination here
 	if(charNum > 1) return;
-
+	//Ascii didnt lie between 128 - 255
+	//Left arrow 129
+	//right arrow 130
+	//Up arrow 131
+	//Down arrow 132
 	switch(combination[0]){
-		case 1:
+		case 129:
 			//left
-			value[current_selection] = max(0, value[current_selection] - 1);
+			if(current_selection == 0){
+				current_led = (current_led + 1) % (MAX_LED);
+				current_selection = 0;
+			}
+			else{//if value > 1
+				ws2812.LED_Data[current_led][current_selection - 1] = max(0, ws2812.LED_Data[current_led][current_selection - 1] - 1);
+			}
 			break;
-		case 2:
+		case 130:
 			//right
-			value[current_selection] = min(255, value[current_selection] + 1);
-		case 3:
-			//up
-			current_selection = (current_selection - 1) % current_selection;
+			if(current_selection == 0){
+				current_led = (current_led - 1) % (MAX_LED);
+				current_selection = 0;
+			}
+			else{//if value > 1
+				ws2812.LED_Data[current_led][current_selection - 1] = min(255, ws2812.LED_Data[current_led][current_selection - 1] + 1);
+			}
 			break;
-		case 4:
-			current_selection = (current_selection + 1) % current_selection;
+		case 131:
+			//up
+			current_selection = (current_selection - 1) % (SELECTION + 1);
+			break;
+		case 132:
+			current_selection = (current_selection + 1) % (SELECTION + 1);
 
 	}
 	led_page_update();
 }
 
 void led_page_update(){
-	char temp[20];
-	for(int i = 0; i < 3; i++){
-		sprintf(temp,
-				i == current_selection ? "%s	<-- %d" :"%s	    %d",
-				color[i], value[i]);
-		ssd1306_SetCursor(0, 20 + 15* (i));
-		ssd1306_WriteString(temp, Font_7x10, White);
-	}
+
 }
