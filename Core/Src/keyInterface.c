29cb0baebc3d23020e053eb501c9339cad1df580
Harrison <sloab@ust.hk>
2022-01-11 19:36:15 +0800

    update readme

--- Core/Src/keyInterface.c
+++ Core/Src/keyInterface.c
@@ -211,19 +211,19 @@ void buttons_init(Button* btns, const int len) {
     btns[67].keycode = KEY_UP;
 
     button_init(&btns[68], Pin(COL0), Pin(ROW5));
-    // btns[68].keycode = KEY_LEFTCTRL;  //buggy on mac
+    btns[68].keycode = KEY_LEFTCTRL;  //buggy on mac
     button_init(&btns[69], Pin(COL1), Pin(ROW5));
-    // btns[69].keycode = KEY_LEFTMETA;  //buggy on mac
+    btns[69].keycode = KEY_LEFTMETA;  //buggy on mac
     button_init(&btns[70], Pin(COL2), Pin(ROW5));
-    // btns[70].keycode = KEY_LEFTALT;  //buggy on mac
+    btns[70].keycode = KEY_LEFTALT;  //buggy on mac
     button_init(&btns[71], Pin(COL3), Pin(ROW5));
     btns[71].keycode = KEY_SPACE;
     button_init(&btns[72], Pin(COL4), Pin(ROW5));
-    // btns[72].keycode = KEY_RIGHTALT;  //buggy on mac
+    btns[72].keycode = KEY_RIGHTALT;  //buggy on mac
     button_init(&btns[73], Pin(COL5), Pin(ROW5));
     btns[73].keycode = KEY_NONE;  //fn
     button_init(&btns[74], Pin(COL6), Pin(ROW5));
-    // btns[74].keycode = KEY_RIGHTCTRL;  //buggy on mac
+    btns[74].keycode = KEY_RIGHTCTRL;  //buggy on mac
     button_init(&btns[75], Pin(COL7), Pin(ROW5));
     btns[75].keycode = KEY_LEFT;
     button_init(&btns[76], Pin(COL8), Pin(ROW5));
